8564a499d1bd01745f94f3dc4cffa7bccdb9fbca
tm-const <dev@mannyquintanilla.com>
2020-05-31 23:47:49 -0400

    Initial Commit

---
+++ untitled.php
@@ -0,0 +1,105 @@
+<header class="main-header main-header-overlay" data-sticky-header="true">
+    <div class="lqd-sticky-placeholder hide" style="height: 88px;"></div>
+    <div class="lqd-sticky-placeholder hide" style="height: 68px;"></div><div class="lqd-sticky-placeholder hide" style="height: 88px;"></div><div class="lqd-sticky-placeholder hide" style="height: 88px;"></div><div class="lqd-sticky-placeholder hide" style="height: 88px;"></div><div class="mainbar-wrap">
+  <div class="mainbar-wrap first">
+    <div class="megamenu-hover-bg"></div>
+    <div class="container mainbar-container">
+      <div class="mainbar">
+        <div class="row mainbar-row align-items-lg-stretch padding-left-right-lg">
+          <div class="col">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="/" rel="home" style="padding-left:0;">
+              <span class="navbar-brand-inner">
+              <img style="width:200px;" class="logo-dark" src="/wp-content/uploads/2020/05/TRAFFICPILOT_hortizontal-lineup-copy.png" alt="Traffic Pilot Logo">
+              <img style="width:200px;" class="logo-light" src="/wp-content/uploads/2020/05/TRAFFICPILOT_hortizontal-lineup-copy.png" alt="Traffic Pilot Logo">
+              <img style="width:200px;" class="logo-sticky" src="/wp-content/uploads/2020/05/TRAFFICPILOT_hortizontal-lineup-copy.png" alt="Traffic Pilot Logo">
+              <img style="width:60px;" class="mobile-logo-default" src="https://trafficpilot.com/wp-content/uploads/2020/04/TP_WHITE.png" alt="Traffic Pilot Logo">
+              <img style="width:200px;" class="logo-default" src="/wp-content/uploads/2020/05/TRAFFICPILOT_hortizontal-lineup-copy.png" alt="Traffic Pilot Logo">
+              </span>
+              </a>
+              <div style="    display: flex;
+    justify-content: space-around;
+    flex-direction: row-reverse;">
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="navbar-toggle collapsed nav-trigger style-mobile" data-toggle="collapse" data-target="#main-header-collapse" aria-expanded="false" data-changeclassnames="{ &quot;html&quot;: &quot;mobile-nav-activated overflow-hidden&quot; }">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="bars">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+                </span>
+                </button>
+                
+              </div>
+            </div>
+          </div>
+          <div class="col text-right">
+            <div class="collapse navbar-collapse" id="main-header-collapse" aria-expanded="false" role="tablist" >
+              <ul class="main-nav main-nav-hover-underline-1 nav align-items-lg-stretch justify-content-lg-end " >
+                <li>
+                  <a href="/">
+                  <span class="link-icon"></span>
+                  <span class="link-txt">
+                  <span class="link-ext"></span>
+                  <span class="txt color-white-nav">Home</span>
+                  </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/reviews">
+                  <span class="link-icon"></span>
+                  <span class="link-txt">
+                  <span class="link-ext"></span>
+                  <span class="txt color-white-nav">Reviews</span>
+                  </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/blog">
+                  <span class="link-icon"></span>
+                  <span class="link-txt">
+                  <span class="link-ext"></span>
+                  <span class="txt color-white-nav">Blog</span>
+                  </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="header-module nav-scroll-btn-desktop hide">
+              <a href="https://my.trafficpilot.com/offers/9TaE92wP/checkout" class="btn btn-solid btn-sm round btn-solid border-thick btn-gradient color-secondary font-size-14" target="_blank" style="background-color:#21425d00!important;border-color:#21425d!important;">
+                <span>
+                  <span class="btn-gradient-bg"></span>
+                  <span class="btn-txt">Sign Up</span>
+                  <span class="btn-gradient-bg btn-gradient-bg-hover"></span>
+                </span>
+              </a>
+            </div>
+            <div class="header-module">
+              <a href="https://my.trafficpilot.com/products/traffic-pilot-full-course" class="btn btn-solid btn-sm round btn-solid border-thick btn-gradient color-secondary font-size-14" target="_blank" style="background-color:#21425d00!important;border-color:#21425d!important;border:1px solid #0793ff !important;">
+                <span>
+                  <span class="btn-gradient-bg"></span>
+                  <span class="btn-txt">Sign In</span>
+                  <span class="btn-gradient-bg btn-gradient-bg-hover"></span>
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script>
+if ($(window).width() <= 1200 && $(window).scroll(function()) {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 70) {
+    $(".nav-scroll-btn-desktop").removeClass("hide");
+    $("div#main-header-collapse").addClass("hide");
+  } else {
+    $(".nav-scroll-btn-desktop").addClass("hide");
+    $("div#main-header-collapse").removeClass("hide");
+  }
+});
+</script>
